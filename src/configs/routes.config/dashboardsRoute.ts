c9775eaa7e8d58b32bc66d441b7fda53a2b0f323
Dilip BK <dilipbk.dev@gmail.com>
2024-10-15 23:33:59 +0545

    project initialized

---
+++ src/configs/routes.config/dashboardsRoute.ts
@@ -0,0 +1,27 @@
+import { lazy } from 'react'
+import { DASHBOARDS_PREFIX_PATH } from '@/constants/route.constant'
+import { ADMIN, USER } from '@/constants/roles.constant'
+import type { Routes } from '@/@types/routes'
+
+const dashboardsRoute: Routes = [
+    {
+        key: 'dashboard.ecommerce',
+        path: `${DASHBOARDS_PREFIX_PATH}/ecommerce`,
+        component: lazy(() => import('@/views/dashboards/EcommerceDashboard')),
+        authority: [ADMIN, USER],
+        meta: {
+            pageContainerType: 'contained',
+        },
+    },
+    {
+        key: 'dashboard.project',
+        path: `${DASHBOARDS_PREFIX_PATH}/project`,
+        component: lazy(() => import('@/views/dashboards/ProjectDashboard')),
+        authority: [ADMIN, USER],
+        meta: {
+            pageContainerType: 'contained',
+        },
+    },
+]
+
+export default dashboardsRoute
