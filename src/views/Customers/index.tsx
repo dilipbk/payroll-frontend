ee1c147a2b59af42734c79109f31bceb89a6bca5
Dilip BK <dilipbk.dev@gmail.com>
2024-10-17 21:16:30 +0545

    customer route updated

--- src/views/Customers/index.tsx
+++ src/views/Customers/index.tsx
@@ -1,5 +1,5 @@
 const CustomerList = () => {
-    return <div>This is customer page</div>
+    return <div>This is customer page and it is updated for deployment</div>
 }
 
 export default CustomerList
